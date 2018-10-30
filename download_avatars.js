48da4e77549ee3acf20f9e35f51261856ad92d83
Martin Lorenzo Tanchanco <tanchanco@gmail.com>
2018-10-30 18:39:22 +0000

    Figured out to parse key/value from an array of objects

--- download_avatars.js
+++ download_avatars.js
@@ -11,14 +11,22 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
-
+  //format data and extract avatar_url
+  var avatarURL = [];
   request(options, function(err, res, body) {
-
     cb(err, body);
+    var targetBody = JSON.parse(body);
+    console.log(targetBody);
+    targetBody.forEach(function (element) {
+      avatarURL.push(element.avatar_url);
+    });
+    console.log(avatarURL);
   });
 }
 
+//Test code
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
